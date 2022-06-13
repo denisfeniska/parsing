a48c6bf3ebe0d61818278d5271e97e7c2961ef90
denisfeniska <2884685@mail.ru>
2022-06-14 00:47:20 +0300

    the final part of sorted.py

--- test_stepik_course/spreadsheets/sorted.py
+++ test_stepik_course/spreadsheets/sorted.py
@@ -3,8 +3,7 @@ import re
 
 
 products = pd.read_excel("test_stepik_course/spreadsheets/trekking1.xlsx")
-products.set_index('Unnamed: 0', inplace=True)
-sorted_products = products.sort_values(by='ККал на 100', ascending=False).index
-# print(sorted_products)
-for i in sorted_products:
-    print(i)
+print(*products.sort_values(
+	by=['ККал на 100', 'Unnamed: 0'],
+	ascending=[False, True]
+	)['Unnamed: 0'].to_list(), sep='\n')
