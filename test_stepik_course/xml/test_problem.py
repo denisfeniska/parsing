e38b000e7100a6a0c15f68b7bc089f12944b4f0a
denisfeniska <2884685@mail.ru>
2022-06-18 23:21:21 +0300

    solve test project

---
+++ test_stepik_course/xml/test_problem.py
@@ -0,0 +1,8 @@
+import xmltodict
+
+fin = open('test_stepik_course/xml/map1.osm', 'r', encoding='utf8')
+xml = fin.read()
+fin.close()
+
+parsedxml = xmltodict.parse(xml)
+print(parsedxml['osm']['node'][100]['@id'])
