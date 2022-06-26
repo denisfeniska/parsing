de0b45a2ddd0cfe1f077866dfdb3a6f9b344a554
denisfeniska <2884685@mail.ru>
2022-06-26 11:17:35 +0300

    working with html in python

--- test_stepik_course/publication of data/generation html with python.py
+++ test_stepik_course/publication of data/generation html with python.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 from numpy import matrix
-=======
->>>>>>> 9cbf7ab3053d3f31937fc254294a0f456cbca17e
 from yattag import Doc
 
 doc, tag, text = Doc().tagtext()
 
-<<<<<<< HEAD
 matrix = [[y*x for y in range(1, 11)] for x in range(1, 11)]
-=======
->>>>>>> 9cbf7ab3053d3f31937fc254294a0f456cbca17e
 
 with tag('html'):
     with tag('body'):
@@ -18,7 +12,6 @@ with tag('html'):
         # with tag('a', href='/my-url'):
         #     text('some link')
         with tag('table'):
-<<<<<<< HEAD
             for i in range(10):
                 with tag('tr'):
                     for j in range(10):
@@ -27,11 +20,3 @@ with tag('html'):
 
 result = doc.getvalue()
 print(result)
-=======
-            with tag('tr'):
-                with tag('td'):
-                    text(matr[i][j])
-
-result = doc.getvalue()
-print(result)
->>>>>>> 9cbf7ab3053d3f31937fc254294a0f456cbca17e
